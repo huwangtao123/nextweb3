88bceca18f9658f2ad6b383543eb3b12f3e41951
Tao Wang <huwangtao123@gmail.com>
2022-12-18 21:15:42 -0800

    After create new project, update the wagmi to 0.9.0 and fix the breaking change

--- packages/frontend/pages/_app.tsx
+++ packages/frontend/pages/_app.tsx
@@ -4,15 +4,16 @@ import NextHead from 'next/head';
 import '../styles/globals.css';
 
 // Imports
-import { chain, createClient, WagmiConfig, configureChains } from 'wagmi';
+import { createClient, WagmiConfig, configureChains } from 'wagmi';
 import { alchemyProvider } from 'wagmi/providers/alchemy';
 import { publicProvider } from 'wagmi/providers/public';
+import { mainnet, polygon, optimism, arbitrum } from 'wagmi/chains';
 
 import '@rainbow-me/rainbowkit/styles.css';
 import {
   getDefaultWallets,
   RainbowKitProvider,
-  Chain,
+  Chain
 } from '@rainbow-me/rainbowkit';
 
 import { useIsMounted } from '../hooks';
@@ -27,29 +28,29 @@ const hardhatChain: Chain = {
   nativeCurrency: {
     decimals: 18,
     name: 'Hardhat',
-    symbol: 'HARD',
+    symbol: 'HARD'
   },
   network: 'hardhat',
   rpcUrls: {
-    default: 'http://127.0.0.1:8545',
+    default: { http: ['http://127.0.0.1:8545'] }
   },
-  testnet: true,
+  testnet: true
 };
 
 const { chains, provider } = configureChains(
-  [chain.mainnet, chain.polygon, chain.optimism, chain.arbitrum, hardhatChain],
+  [mainnet, polygon, optimism, arbitrum, hardhatChain],
   [alchemyProvider({ apiKey: alchemyId }), publicProvider()]
 );
 
 const { connectors } = getDefaultWallets({
   appName: 'create-web3',
-  chains,
+  chains
 });
 
 const wagmiClient = createClient({
   autoConnect: true,
   connectors,
-  provider,
+  provider
 });
 
 const App = ({ Component, pageProps }: AppProps) => {
